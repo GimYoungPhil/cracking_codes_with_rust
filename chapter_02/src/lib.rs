38fc8ddbcb907856dca57c51e97db67a69ec3264
YoungPhilGim <youngphil.gim@gmail.com>
2023-05-23 16:59:18 +0900

    fixed

--- chapter_02/src/lib.rs
+++ chapter_02/src/lib.rs
@@ -2,19 +2,42 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+const SYMBOLS: &'static str = "ABCDEFGHIJKLMLOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.";
+
 pub enum Mode {
     Encrypt,
     Decrypt,
 }
 
+pub fn rotated_key(key: usize, length: usize) -> usize {
+    key % length
+}
+
+pub fn rotated_key_1(key: i128, length: usize) -> i128 {
+    key % (length as i128)
+}
+
+pub fn rotated_key_2(key: i128, length: usize) -> i128 {
+    key.rem_euclid(length as i128)
+}
+
+pub fn test_message(message: &str) {
+    for ch in message.chars() {
+        match SYMBOLS.find(ch) {
+            Some(index) => println!("char: {}, index: {}", ch, index),
+            None => println!("char: {}, index: None!!", ch),
+        }
+    }
+}
+
+/*
 pub fn caesar_cipher(key: usize, mode: Mode, message: &str) -> String {
+    let symbols_length = SYMBOLS.len();
+    let message_length = message.len();
+    let mut translated: Vec<u8> = Vec::with_capacity(message_length);
 
-    let symbols = "ABCDEFGHIJKLMLOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.";
-    let symbols_length = symbols.len();
-    let mut translated: Vec<u8> = Vec::with_capacity(message.len());
-
-    for ch in message.bytes() {
-        match symbols.find(ch as char) {
+    for ch in message.chars() {
+        match SYMBOLS.find(ch) {
             Some(symbol_index) => {
                 let mut translated_index: usize;
 
@@ -33,7 +56,6 @@ pub fn caesar_cipher(key: usize, mode: Mode, message: &str) -> String {
                     Some(s) => translated.push(s),
                     None => (),
                 }
-
             },
             None => {
                 translated.push(ch);
@@ -41,8 +63,9 @@ pub fn caesar_cipher(key: usize, mode: Mode, message: &str) -> String {
         }
     }
 
-    String::from("result")
+    String::from_utf8(translated).expect("Invalid UTF-8")
 }
+*/
 
 #[cfg(test)]
 mod tests {
@@ -52,5 +75,93 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn it_key_0() {
+        assert_eq!(rotated_key(0, 26), 0);
+        assert_eq!(rotated_key(1, 26), 1);
+        assert_eq!(rotated_key(26, 26), 0);
+        assert_eq!(rotated_key(27, 26), 1);
+        assert_eq!(rotated_key(52, 26), 0);
+    }
+
+    #[test]
+    fn it_key_1() {
+        assert_eq!(rotated_key_1(0, 26), 0);
+        assert_eq!(rotated_key_1(1, 26), 1);
+        assert_eq!(rotated_key_1(26, 26), 0);
+        assert_eq!(rotated_key_1(27, 26), 1);
+        assert_eq!(rotated_key_1(52, 26), 0);
+
+        assert_eq!(rotated_key_1(-1, 26), -1);
+        assert_eq!(rotated_key_1(-26, 26), 0);
+        assert_eq!(rotated_key_1(-27, 26), -1);
+        assert_eq!(rotated_key_1(-52, 26), 0);
+    }
+
+    #[test]
+    fn it_key_2() {
+        assert_eq!(rotated_key_2(0, 26), 0);
+        assert_eq!(rotated_key_2(1, 26), 1);
+        assert_eq!(rotated_key_2(25, 26), 25);
+
+        assert_eq!(rotated_key_2(26, 26), 0);
+        assert_eq!(rotated_key_2(27, 26), 1);
+        assert_eq!(rotated_key_2(51, 26), 25);
+
+        assert_eq!(rotated_key_2(-26, 26), 0);
+        assert_eq!(rotated_key_2(-25, 26), 1);
+        assert_eq!(rotated_key_2(-1, 26), 25);
+
+        assert_eq!(rotated_key_2(-52, 26), 0);
+        assert_eq!(rotated_key_2(-51, 26), 1);
+        assert_eq!(rotated_key_2(-27, 26), 25);
+    }
+
+    #[test]
+    fn it_works_1() {
+        assert_eq!(7_i64.div_euclid(4), 1);
+        assert_eq!(7_i64.div_euclid(-4), -1);
+        assert_eq!((-7_i64).div_euclid(4), -2);
+        assert_eq!((-7_i64).div_euclid(-4), 2);
+        
+        // assert_eq!(7_i64.div_euclid(0), -1);
+        // assert_eq!((-7_i64).div_euclid(0), 2);
+
+        assert_eq!(7_i64.rem_euclid(4), 3);
+        assert_eq!(7_i64.rem_euclid(-4), 3);
+        assert_eq!((-7_i64).rem_euclid(4), 1);
+        assert_eq!((-7_i64).rem_euclid(-4), 1);
+    }
+
+    #[test]
+    fn it_works_2() {
+        assert_eq!(7_i64.div_euclid(1), 7);
+        assert_eq!(7_i64.rem_euclid(1), 0);
+
+        assert_eq!(7_i64.div_euclid(-1), -7);
+        assert_eq!(7_i64.rem_euclid(-1), 0);
+
+        assert_eq!((-7_i64).div_euclid(1), -7);
+        assert_eq!((-7_i64).rem_euclid(1), 0);
+
+        assert_eq!((-7_i64).div_euclid(-1), 7);
+        assert_eq!((-7_i64).rem_euclid(-1), 0);
+    }
+
+    #[test]
+    fn it_works_3() {
+        assert_eq!(127_i8.div_euclid(1), 127);
+        assert_eq!(127_i8.rem_euclid(1), 0);
+
+        assert_eq!(127_i8.div_euclid(-1), -127);
+        assert_eq!(127_i8.rem_euclid(-1), 0);
+
+        assert_eq!((-128_i8).div_euclid(1), -128);
+        assert_eq!((-128_i8).rem_euclid(1), 0);
+
+        // assert_eq!((-128_i8).div_euclid(-1), 128);
+        // assert_eq!((-128_i8).rem_euclid(-1), 0);
     }
 }
--- cracking_codes/src/main.rs
+++ cracking_codes/src/main.rs
@@ -1,6 +1,9 @@
-use chapter_01;
+// use chapter_01;
+use chapter_02;
 
 fn main() {
     let message = "There can keep a secret, if two of them are dead.";
-    println!("{}", chapter_01::reverse_chiper(message));
+    // println!("{}", chapter_01::reverse_chiper(message));
+
+    chapter_02::test_key(30, 20);
 }
