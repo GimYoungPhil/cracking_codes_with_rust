cb17a937b5d81843d0d39dc869f1ee4414befd22
YoungPhilGim <youngphil.gim@gmail.com>
2023-05-26 15:15:18 +0900

    fixed

--- cracking_codes/src/main.rs
+++ cracking_codes/src/main.rs
@@ -15,14 +15,20 @@ fn main() {
     // chapter_00::euclid(-128);
     // chapter_00::display_rem(5);
 
+    // let integers: [(i8, i8); 4] = [
+    //     (11_i8, 26_i8),
+    //     (11_i8, -26_i8),
+    //     (-11_i8, 26_i8),
+    //     (-11_i8, -26_i8),
+    // ];
     let integers: [(i8, i8); 4] = [
-        (13_i8, 5_i8),
-        (13_i8, -5_i8),
-        (-13_i8, 5_i8),
-        (-13_i8, -5_i8),
+        (i8::MAX, 1_i8),
+        (i8::MAX, -1_i8),
+        (i8::MIN, 1_i8),
+        (i8::MIN, -1_i8),
     ];
-    chapter_00::display_div(&integers);
-    chapter_00::display_div_euclid(&integers);
+    // chapter_00::display_div(&integers);
+    // chapter_00::display_div_euclid(&integers);
     chapter_00::display_checked_div(&integers);
     chapter_00::display_checked_div_euclid(&integers);
 }
