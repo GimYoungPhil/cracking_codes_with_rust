0d0564001171fb0d0e1c16dddfbbabbf3d404719
YoungPhilGim <youngphil.gim@gmail.com>
2023-06-01 16:56:26 +0900

    fixed

--- chapter_02/src/lib.rs
+++ chapter_02/src/lib.rs
@@ -3,33 +3,36 @@ pub enum Mode {
     Decrypt,
 }
 
+const SYMBOLS: &'static str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.";
+
 pub fn caesar_cipher(key: i32, mode: Mode, message: &str) -> String {
 
-    let symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.";
-
-    let symbols_length = symbols.len() as i32;
+    let symbols_length = SYMBOLS.len() as i32;
 
     let cipher_key = key % symbols_length;
 
     let mut translated: String = String::with_capacity(message.len());
 
     for ch in message.chars() {
-        match symbols.find(ch) {
+        print!("ch: {}, ", ch);
+        match SYMBOLS.find(ch) {
             Some(symbol_index) => {
+                print!("in: {:>2}, ", symbol_index);
                 let moved_index = match mode {
                     Mode::Encrypt => symbol_index as i32 + cipher_key,
                     Mode::Decrypt => symbol_index as i32 - cipher_key,
                 };
 
                 let translated_index = (moved_index % symbols_length) as usize;
-
-                translated.push_str(&symbols[translated_index..translated_index+1]);
+                print!("ou: {:>2}, ", translated_index);
+                translated.push_str(&SYMBOLS[translated_index..translated_index+1]);
             },
             None => {
                 println!();
                 translated.push(ch);
             },
         }
+        println!();
     }
 
     translated
