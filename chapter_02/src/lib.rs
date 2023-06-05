9f12b766d8e7c04317e4cbddc5810dc214594cb6
YoungPhilGim <youngphil.gim@gmail.com>
2023-06-05 15:37:36 +0900

    fixed

--- chapter_02/src/lib.rs
+++ chapter_02/src/lib.rs
@@ -1,43 +1,68 @@
-pub enum Mode {
-    Encrypt,
-    Decrypt,
-}
+pub mod caesar {
 
-const SYMBOLS: &'static str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.";
-
-pub fn caesar_cipher(key: i32, mode: Mode, message: &str) -> String {
-
-    let symbols_length = SYMBOLS.len() as i32;
-
-    let cipher_key = key % symbols_length;
-
-    let mut translated: String = String::with_capacity(message.len());
-
-    for ch in message.chars() {
-        // print!("ch: {}, ", ch);
-        match SYMBOLS.find(ch) {
-            Some(symbol_index) => {
-                // print!("in: {:>2}, ", symbol_index);
-                let moved_index = match mode {
-                    Mode::Encrypt => symbol_index as i32 + cipher_key,
-                    Mode::Decrypt => symbol_index as i32 - cipher_key,
-                };
-                // print!("mo: {:>2}, ", moved_index);
-                let translated_index = moved_index.rem_euclid(symbols_length) as usize;
-                // print!("ou: {:>2}, ", translated_index);
-                translated.push_str(&SYMBOLS[translated_index..translated_index+1]);
-            },
-            None => {
-                // println!();
-                translated.push(ch);
-            },
-        }
-        // println!();
+    pub struct Cipher {
+        key: i32,
     }
 
-    translated
-}
+    enum Mode {
+        Encrypt,
+        Decrypt,
+    }
 
+    const SYMBOLS: &'static str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.";
+
+    impl Cipher {
+
+        pub fn with_key(key: i32) -> Cipher {
+            Cipher { key }
+        }
+
+        fn translate_message(&self, message: &str, mode: Mode) -> String {
+
+            let symbols_length = SYMBOLS.len() as i32;
+
+            let cipher_key = self.key % symbols_length;
+
+            let mut translated = String::with_capacity(message.len());
+
+            for ch in message.chars() {
+                
+                match SYMBOLS.find(ch) {
+                    Some(symbol_index) => {
+
+                        let moved_index = match mode {
+                            Mode::Encrypt => symbol_index as i32 + cipher_key,
+                            Mode::Decrypt => symbol_index as i32 - cipher_key,
+                        };
+
+                        let translated_index: usize = moved_index.rem_euclid(symbols_length) as usize;
+                        translated.push_str(&SYMBOLS[translated_index..translated_index+1]);
+
+                        println!("ch: {:>2}, in: {:>2}, mo: {:>2}, ou: {:>2}", ch, symbol_index, moved_index, translated_index);
+                    },
+                    None => {
+                        translated.push(ch);
+
+                        println!("ch: {:>2}, No match", ch);
+                    },
+                }
+            }
+
+            translated
+        }
+
+        pub fn encrypt_message(&self, message: &str) -> String {
+            self.translate_message(message, Mode::Encrypt)
+        }
+
+        pub fn decrypt_message(&self, message: &str) -> String {
+            self.translate_message(message, Mode::Decrypt)
+        }
+
+
+    }
+
+}
 
 #[cfg(test)]
 mod tests {
--- cracking_codes/src/main.rs
+++ cracking_codes/src/main.rs
@@ -1,14 +1,15 @@
 // use chapter_00;
 // use chapter_01;
-use chapter_02::{self, Mode};
+use chapter_02::caesar::Cipher;
 
 fn main() {
-    let key = 13;
     let message = "This is my secret message.";
 
-    let encode = chapter_02::caesar_cipher(key, Mode::Encrypt, message);
-    println!("{}", encode);
+    let cipher = Cipher::with_key(13);
 
-    let decode = chapter_02::caesar_cipher(key, Mode::Decrypt, &encode);
-    println!("{}", decode);
+    let encoding = cipher.encrypt_message(message);
+    println!("{}", encoding);
+
+    let decoding = cipher.decrypt_message(&encoding);
+    println!("{}", decoding);
 }
